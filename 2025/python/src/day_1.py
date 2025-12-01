e6f78c03c6557641f970ac217d3f52c515aa0b81
Nicola Palmieri <npalmieri@hotmail.com>
2025-12-01 22:02:15 +0100

    soluzione parte 2

--- 2025/python/src/day_1.py
+++ 2025/python/src/day_1.py
@@ -3,47 +3,51 @@ import utils
 def map_fn(line):
     return line
 
-def part_1():
-    rotations = utils.read_input(1,map_fn, True)
+def part_1(rotations):
+    zeros = 0
+    dial = 50
+    for line in rotations:
+        step = -1 if line[0] == "L" else 1
+        amount = int(line[1:])
+        start = dial  # caching starting dial position
+        dial = (start + step * amount) % 100
 
-    pos = 50
-    hits = 0
+        if dial == 0:
+            zeros += 1
 
-    for rot in rotations:
-        direction = rot[0]
-        dist = int(rot[1:])
+    print("Password part_1:", zeros)
 
-        match direction:
-            case "L":
-                pos = (pos - dist) % 100
-            case "R":
-                pos = (pos + dist) % 100
+def part_2(rotations):
+    zeros = 0
+    dial = 50
+    for line in rotations:
+        step = -1 if line[0] == "L" else 1
+        amount = int(line[1:])
+        start = dial  # caching starting dial position
+        dial = (start + step * amount) % 100
 
-        if pos == 0:
-            hits += 1
+        # Each click k moves the dial in position p = (start + step*k) % 100
+        # I want to count all the positions p=0
+        # These are the solution of:
+        # start + step*k = 0 (mod 100)
+        # solving for k:
+        # k = -start * step^-1 (mod 100)
+        # So the first click k that touches 0 is:
+        k_first = (-start * pow(step, -1, 100)) % 100
 
-    print("Password part_1:", hits)
+        if k_first == 0:  # k=0 is an invalid configuration, since I want to check dial positions only after a valid click (k>=1)
+            k_first = 100  # if by chance k=0, the next valid "landing on zero" is for k=100
 
-def part_2():
-    rotations = utils.read_input(1,map_fn, True)
+        # count zero crossings
+        crosszero = 0 if k_first > amount else 1 + (amount - k_first) // 100
 
-    pos = 50
-    hits = 0
+        zeros += crosszero
 
-    for rot in rotations:
-        direction = rot[0]
-        dist = int(rot[1:])
+    print("Password part_2:", zeros)
 
-        match direction:
-            case "L":
-                pos = (pos - dist) % 100
-            case "R":
-                pos = (pos + dist) % 100
 
-        if pos == 0:
-            hits += 1
-
-    print("Password part_2:", hits)
 
 if __name__ == "__main__":
-    part_2()
+    input_rotations = utils.read_input(1, map_fn, False)
+    part_1(input_rotations)
+    part_2(input_rotations)
